95d5bfd5c0a4ea4c2d246400cde8f8f9b1298a74
ali <Alinazim363@gmail.com>
2025-03-29 22:54:21 -0400

    Update parking1.sh

--- src/parking/parking1.sh
+++ src/parking/parking1.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 ../../../../start.sh
 
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
 
 APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
 API_URL="http://api.example.com/ny_parking_tickets?limit=0&app_token=${APP_TOKEN}"
 
 curl -s "$API_URL" -o data.csv
 
-/usr/local/hadoop/bin/hdfs dfs -put data.csv /project1/input/
+/usr/local/hadoop/bin/hdfs dfs -put data.csv /mapreduce-project1/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
   -file mapper1.py -mapper mapper1.py \
   -file reducer1.py -reducer reducer1.py \
-  -input /project1/input/* -output /project1/output/
+  -input /mapreduce-project1/input/* -output /mapreduce-project1/output/
 
-/usr/local/hadoop/bin/hdfs dfs -cat /project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -cat /mapreduce-project1/output/part-00000
 
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /project1/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
 
 ../../../../stop.sh
