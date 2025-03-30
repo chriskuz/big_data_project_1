f271c1f5a6ad8dbaa575536b376488dda0e2c7be
ali <Alinazim363@gmail.com>
2025-03-30 16:22:03 -0400

    updated sh files

--- src/parking/api_test.sh
+++ src/parking/api_test.sh
@@ -1,4 +1,4 @@
 APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
-API_URL="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?$limit=50000"
+#API_URL="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?limit=0&$$app_token=${APP_TOKEN}"
 
-curl -v -H "X-App-Token: $APP_TOKEN" "$API_URL" -o data.csv
+curl -s -H "X-App-Token: $APP_TOKEN" "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?limit=50000" -o data.csv
