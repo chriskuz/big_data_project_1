6d9f98aff02345e2af32e094deaf525a759032d7
ali <Alinazim363@gmail.com>
2025-03-30 16:06:56 -0400

    Update api_test.sh

--- src/parking/api_test.sh
+++ src/parking/api_test.sh
@@ -1,4 +1,4 @@
 APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
 API_URL="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?$limit=50000"
 
-curl -v --fail -H "X-App-Token: $APP_TOKEN" "$API_URL" -o data.csv || { echo "Error: Failed to fetch data"; exit 1; }
+curl -v -H "X-App-Token: $APP_TOKEN" "$API_URL" -o data.csv
