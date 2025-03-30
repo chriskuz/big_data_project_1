e7ff43e989e63b6c8d3cdadd58d601e790cea966
Chris Kuzemka <cmkuzemka@gmail.com>
2025-03-30 18:42:44 -0400

    closed open string. made notes for needed brackets or not. renamed fine.

--- src/nba_shots/get_data.sh
+++ src/nba_shots/get_data.sh
@@ -13,15 +13,15 @@ mkdir -p "$DATA_DIR"
 cd "$DATA_DIR" || { echo: "Failed data directory access"; exit 1;}
 
 # === Download ZIP using curl and API token ===
-echo "Downloading NBA shot logs ZIP from Kaggle
+echo "Downloading NBA shot logs ZIP from Kaggle"
 curl -L -u "${KAGGLE_USER}:${KAGGLE_KEY}" \
   -o "$ZIP_NAME" \
   "https://www.kaggle.com/api/v1/datasets/download/dansbecker/nba-shot-logs"
 
 # === Extract & clean up ===
 echo "Extracting contents"
-unzip -o "$ZIP_NAME"
-rm -f "$ZIP_NAME"
+unzip -o "$ZIP_NAME" #== might need brackets (also above)
+rm -f "$ZIP_NAME" #== might need brackets
 
-
+echo "Done"
 
