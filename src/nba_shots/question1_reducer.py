3a7930ebdfe48971ad65eceb85f1dcc644254ca2
Chris Kuzemka <cmkuzemka@gmail.com>
2025-03-30 22:27:57 -0400

    added some comments.

--- src/nba_shots/question1_reducer.py
+++ src/nba_shots/question1_reducer.py
@@ -2,6 +2,8 @@
 import sys
 from collections import defaultdict
 
+min_fear_scores = {}
+
 current_player_defender = None
 fgm_sum = 0
 attempts = 0
@@ -24,7 +26,7 @@ for line in sys.stdin:
         attempts = 0
 
     fgm_sum += fgm
-    attempts += 1
+    attempts += 1 #adds attempt by 
 
 #last one clause
 if current_player_defender and fgm_sum >= 3:
