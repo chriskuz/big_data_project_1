6f32dec9c850756fff5a4676a7482bd5e43951fb
ali <Alinazim363@gmail.com>
2025-03-30 20:25:01 -0400

    Update api_test.sh

--- src/parking/api_test.sh
+++ src/parking/api_test.sh
@@ -1,2 +1,2 @@
 APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
-curl -v -H "X-App-Token: $APP_TOKEN" "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$limit=100" -o data.csv
+curl -v -H "X-App-Token: $APP_TOKEN" "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$limit=50000" -o data.csv
