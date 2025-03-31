5e3506e1bcdb239ba3546a399c66b3175f92a818
ali <Alinazim363@gmail.com>
2025-03-30 21:52:42 -0400

    Update parking1.sh

--- src/parking/parking1.sh
+++ src/parking/parking1.sh
@@ -4,7 +4,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../../../mapreduce-test/mapreduce-project1/big_data_project_1/src/parking/data.csv /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../../mapreduce-project-data/data.csv /mapreduce-project1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -files mapper1.py,reducer1.py \
 -mapper "python mapper1.py" \
