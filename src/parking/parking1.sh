339f9744b4863acad6894c3365689a2de5487bf1
ali <Alinazim363@gmail.com>
2025-03-30 20:40:19 -0400

    Update parking1.sh

--- src/parking/parking1.sh
+++ src/parking/parking1.sh
@@ -5,10 +5,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
 
-APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
-curl -s -H "X-App-Token: $APP_TOKEN" "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$limit=50000" -o data.csv
-
-/usr/local/hadoop/bin/hdfs dfs -put data.csv /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal data.csv /mapreduce-project1/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
   -file mapper1.py -mapper mapper1.py \
