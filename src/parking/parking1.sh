a3fca060d10432da050f578ab4c56107a985a08a
ali <Alinazim363@gmail.com>
2025-03-30 21:00:36 -0400

    Update parking1.sh

--- src/parking/parking1.sh
+++ src/parking/parking1.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
-../../../../start.sh
 
+../../../../start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
-
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal data.csv /mapreduce-project1/input/
-
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../../../../mapreduce-test/mapreduce-project1/big_data_project_1/src/parking/data.csv /mapreduce-project1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
-  -file mapper1.py -mapper mapper1.py \
-  -file reducer1.py -reducer reducer1.py \
-  -input /mapreduce-project1/input/* -output /mapreduce-project1/output/
-
+-files mapper1.py,reducer1.py \
+-mapper "python mapper1.py" \
+-reducer "python reducer1.py" \
+-input /mapreduce-project1/input/* \
+-output /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /mapreduce-project1/output/part-00000
-
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
-
 ../../../../stop.sh
