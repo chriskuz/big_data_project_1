e6797b44b8a27addb01020d083ad53d9a0424987
ali <Alinazim363@gmail.com>
2025-03-31 17:40:13 -0400

    Create parking3.sh

---
+++ src/parking/parking3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+../../../../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/data.csv /mapreduce-project1/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-files mapper3.py,reducer3.py \
+-mapper "python3 mapper3.py" \
+-reducer "python3 reducer3.py" \
+-input /mapreduce-project1/input/* \
+-output /mapreduce-project1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /mapreduce-project1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
+../../../../stop.sh
