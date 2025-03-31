2d07e62be34d51878a38f3f27ee08096a721f130
ali <Alinazim363@gmail.com>
2025-03-31 16:55:02 -0400

    updated shell scripts

--- src/parking/parking1.sh
+++ src/parking/parking1.sh
@@ -4,11 +4,11 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /mapreduce-project1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal data.csv /mapreduce-project1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/data.csv /mapreduce-project1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -files mapper1.py,reducer1.py \
--mapper "python mapper1.py" \
--reducer "python reducer1.py" \
+-mapper "python3 mapper1.py" \
+-reducer "python3 reducer1.py" \
 -input /mapreduce-project1/input/* \
 -output /mapreduce-project1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /mapreduce-project1/output/part-00000
