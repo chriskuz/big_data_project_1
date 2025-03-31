25ed41ac34b8355dd4509ae3cabe0003c884e1a0
Chris Kuzemka <cmkuzemka@gmail.com>
2025-03-30 22:49:45 -0400

    edited reducer to give proper output

--- src/nba_shots/question1_reducer.py
+++ src/nba_shots/question1_reducer.py
@@ -20,15 +20,25 @@ for line in sys.stdin:
     if current_player_defender != key:
         if current_player_defender and fgm_sum >= 3:
             p, d = current_player_defender 
-            print(f"{p}\t{d}\t{fgm_sum / attempts:.4f}")
+            hit_rate = fgm_sum / attempts
+            if p not in min_fear_scores or hit_rate < min_fear_scores[p][1]:
+                min_fear_scores[p] = (d, hit_rate, fgm_sum, attempts)
+
+            # print(f"{p}\t{d}\t{fgm_sum / attempts:.4f}")
         current_player_defender = key
         fgm_sum = 0
         attempts = 0
 
-    fgm_sum += fgm
-    attempts += 1 #adds attempt by 
+    fgm_sum += fgm #notes boolean presence of fgm and includes as sum. 
+    attempts += 1 #adds attempt up regardless
 
 #last one clause
 if current_player_defender and fgm_sum >= 3:
     p, d = current_player_defender
-    print(f"{p}\t{d}\t{fgm_sum / attempts:.4f}")
+    hit_rate = fgm_sum / attempts
+    if p not in min_fear_scores or hit_rate < min_fear_scores[p][1]:
+        min_fear_scores[p] = (d, hit_rate, fgm_sum, attempts)
+    # print(f"{p}\t{d}\t{fgm_sum / attempts:.4f}")
+
+for player, (defender, fg_pct, fgm, fga) in min_fear_scores.items():
+    print(f"{player}\t{defender}\t{fgm}\t{fga}\t{fg_pct:.4f}")
