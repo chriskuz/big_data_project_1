a61e9eaeb6f5cfbbb2f4aff742fde17522839d2b
ali <Alinazim363@gmail.com>
2025-03-31 16:46:52 -0400

    Create grab_data.sh

---
+++ src/parking/grab_data.sh
@@ -0,0 +1,12 @@
+PROJECT_DIR="/mapreduce-test/mapreduce-project1/big_data_project_1"
+DATA_DIR="$PROJECT_DIR/data"
+
+# === Prep directory ===
+echo "Creating data directory if not already present..."
+mkdir -p "$DATA_DIR"
+cd "$DATA_DIR" || { echo "Failed data directory access"; exit 1; }
+
+APP_TOKEN="tzksBXvIq9l9Ub985hzK3D5HP"
+API_URL="https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?\$limit=50000"
+
+curl -v -H "X-App-Token: $APP_TOKEN" "$API_URL" -o "$DATA_DIR/data.csv"
